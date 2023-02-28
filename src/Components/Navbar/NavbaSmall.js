9af2d5e0753c3ee469e41ba15a61032e88b7f8e2
wwoj <59660633+wwoj@users.noreply.github.com>
2023-02-28 17:05:30 +0100

    Update of style, navbar

--- src/Components/Navbar/NavbaSmall.js
+++ src/Components/Navbar/NavbaSmall.js
@@ -2,7 +2,8 @@ import { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import AniasBalloonLogo from '../../Pictures/AniaBalloons_Logo.png'
 import Dropdown from '../Dropdown';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHippo, faImage, faCakeCandles,faAddressCard,faGifts } from '@fortawesome/free-solid-svg-icons'
 function NavbarSmall (props){
   const ref = useRef(null);
   const [showMenu, setShowMenu] = useState(false);
@@ -13,45 +14,58 @@ function NavbarSmall (props){
             <img src={AniasBalloonLogo} alt="Logo" style={{ width: "25vw" }} />
           </div>
           <div>
-            <button onClick={() => {
-            setShowMenu(!showMenu);
-          }}>
-            <span
-            ref={ref}
-            className={`close-mark${showMenu ? " transform-main" : ""}`}
-          ></span>
+            <button
+              onClick={() => {
+                setShowMenu(!showMenu);
+              }}
+            >
+              <span
+                ref={ref}
+                className={`close-mark${showMenu ? " transform-main" : ""}`}
+              ></span>
             </button>
           </div>
         </nav>
-        <div className={`navbar-list-small ${showMenu ? "navbar-small-show" : ""}`}>
+        <div
+          className={`navbar-list-small ${showMenu ? "navbar-small-show" : ""}`}
+        >
+         
           <ul>
-          <li className="">
-            <NavLink className="" end to="/">
-              Animal Balloon
-            </NavLink>
-          </li>
-          <li className="">
-            <NavLink className="" end to="/backdrops">
-              Photobackdrops
-            </NavLink>
-          </li>
-          <li className="">
-            <NavLink className="" end to="/">
-              Decorations
-            </NavLink>
-          </li>
-          <li className="">
-            <NavLink className="" end to="/">
-              Events
-            </NavLink>
-          </li>
-          <li className="">
-            <NavLink className="" end to="/">
-              Contact
-            </NavLink>
-          </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faHippo} /></div>
+              <span>Animal Balloon</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/backdrops">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faImage} /></div>
 
+              <span>Photobackdrops</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faGifts} /></div>
+
+              <span>Decorations</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faCakeCandles} /></div>
+
+              <span>Events</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faAddressCard} /></div>
+                <span>Contact</span>
+              </NavLink>
+            </li>
           </ul>
+          <div></div>
         </div>
       </div>
     );
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "../Styles/footer.scss";
 import Instagram from "../Pictures/Brands/instagram.svg";
 import Facebook from "../Pictures/Brands/facebook.svg";
+import Youtube from "../Pictures/Brands/youtube.svg";
+
 
 
 
@@ -30,30 +32,12 @@ const Footer = (props) => {
           <a href="https://www.instagram.com/aniasballoonworld/?igshid=YmMyMTA2M2Y%3D&fbclid=IwAR11EcgMLwIDxYHawiz_EeGLG1eyNaDA3pmJePfVMsDLv0KGaN_ls83Is8s" target="_blank">
             <img src={Instagram} alt="instagram" />
           </a>
+          <a href="https://www.instagram.com/aniasballoonworld/?igshid=YmMyMTA2M2Y%3D&fbclid=IwAR11EcgMLwIDxYHawiz_EeGLG1eyNaDA3pmJePfVMsDLv0KGaN_ls83Is8s" target="_blank">
+            <img src={Youtube} alt="youtube" />
+          </a>
         </div>
 
-        {/* <ul className="inner-footer">
-              
-                <li>
-                    <ul>
-                        <li className="column-head">{props.t("footer_Contact")}</li>
-                        <li><a href="mailto:azygmunt@att.net">azygmunt@att.net</a></li>
-                        <li> <a href='tel:+1(630)-290-8175'>+1(630)-290-8175</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <li className="column-head">{props.t("footer_Media")}</li>
-                        <li><span><a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagramSquare} size="2x" /></a></span>
-                            <span><a href='https://www.facebook.com>'><FontAwesomeIcon icon={faFacebookSquare} size="2x" /></a></span>
-                            <span><a href="https://www.youtube.com"><FontAwesomeIcon icon={faYoutubeSquare} size="2x" /></a></span></li>
-
-                    </ul>
-                </li>
-            </ul>
-            <div className='ds-flex-end align-center footer-creator'>
-                <span>Created by: wojcik.wojciechp@gmail.com</span>
-            </div> */}
+        
       </footer>
     );
 }
