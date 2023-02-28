1bc9ef27ee3206c9d01ae93cb4f216434da33139
wojcikewelina <gargula.ewe@gmail.com>
2023-02-28 20:41:48 +0100

    gallery - media querry update

---
+++ src/Components/Navbar/NavbaSmall.js
@@ -0,0 +1,74 @@
+import { useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+import AniasBalloonLogo from '../../Pictures/AniaBalloons_Logo1.png'
+import "../../Styles/navbar.scss"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHippo, faImage, faCakeCandles,faAddressCard,faGifts } from '@fortawesome/free-solid-svg-icons'
+function NavbarSmall (props){
+  const ref = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
+    return (
+      <div>
+        <nav>
+          <div className="header-logo-container">
+            <img src={AniasBalloonLogo} alt="Logo" className="img-logo"/>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                setShowMenu(!showMenu);
+              }}
+            >
+              <span
+                ref={ref}
+                className={`close-mark${showMenu ? " transform-main" : ""}`}
+              ></span>
+            </button>
+          </div>
+        </nav>
+        <div
+          className={`navbar-list-small ${showMenu ? "navbar-small-show" : ""}`}
+        >
+         
+          <ul>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faHippo} /></div>
+              <span>Animal Balloon</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/backdrops">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faImage} /></div>
+
+              <span>Photobackdrops</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faGifts} /></div>
+
+              <span>Decorations</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faCakeCandles} /></div>
+
+              <span>Events</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" end to="/">
+              <div className="navbar-small-container-svg"><FontAwesomeIcon icon={faAddressCard} /></div>
+                <span>Contact</span>
+              </NavLink>
+            </li>
+          </ul>
+          <div></div>
+        </div>
+      </div>
+    );
+}
+export default NavbarSmall;
---
+++ src/Components/Navbar/NavbarBig.js
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import AniasBalloonLogo from '../../Pictures/AniaBalloons_Logo1.png'
+import Dropdown from '../Dropdown';
+const NavbarBig = (props)=>{
+    return (
+      <nav>
+        <div className="header-logo-container">
+          <img className="img-logo" src={AniasBalloonLogo} alt="" />
+        </div>
+        <ul
+          className="header-list"
+          onChange={(e) => {
+            let language = e.target.value.toLowerCase();
+            props.i18n.changeLanguage(language);
+          }}
+        >
+          <li className="nav-button">
+            <NavLink className="" end to="/">
+              Animal Balloon
+            </NavLink>
+          </li>
+          <li className="nav-button">
+            <NavLink className="" end to="/backdrops">
+              Photobackdrops
+            </NavLink>
+          </li>
+          <li className="nav-button">
+            <NavLink className="" end to="/">
+              Decorations
+            </NavLink>
+          </li>
+          <li className="nav-button">
+            <NavLink className="" end to="/">
+              Events
+            </NavLink>
+          </li>
+          <li className="nav-button">
+            <NavLink className="" end to="/">
+              Contact
+            </NavLink>
+          </li>
+
+          <li>
+            <Dropdown
+              selected={props.selected}
+              setSelected={props.setSelected}
+            />
+          </li>
+        </ul>
+      </nav>
+    );
+}
+export default NavbarBig;
